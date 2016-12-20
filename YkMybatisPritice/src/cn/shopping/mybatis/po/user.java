60bc196f9432f1996aa432eee55d0af331c6c1b7
yk <ykredhat@163.com>
2016-12-20 19:35:16 +0800

    first commit

---
+++ YkMybatisPritice/src/cn/shopping/mybatis/po/user.java
@@ -0,0 +1,8 @@
+package cn.shopping.mybatis.po;
+
+/**
+ * Created by tpusers on 2016/12/20.
+ */
+public class user {
+    private
+}
---
+++ YkMybatisPritice/test/cn/shopping/mybatis/usertest.java
@@ -0,0 +1,43 @@
+package cn.shopping.mybatis;
+
+
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.InputStream;
+
+/**
+ * Created by tpusers on 2016/12/20.
+ */
+public class usertest {
+    private SqlSessionFactory sqlSessionFactory;
+
+    // 此方法是在执行testFindUserById之前执行
+    @Before
+    public void setUp() throws Exception {
+        // 创建sqlSessionFactory
+
+        // mybatis配置文件
+        String resource = "SqlMapConfig.xml";
+        // 得到配置文件流
+        InputStream inputStream = Resources.getResourceAsStream(resource);
+
+        // 创建会话工厂，传入mybatis的配置文件信息
+        sqlSessionFactory = new SqlSessionFactoryBuilder()
+                .build(inputStream);
+    }
+
+    //用户信息的综合 查询
+    @Test
+    public void testFindUserByUsername() throws Exception {
+        SqlSession sqlSession = sqlSessionFactory.openSession();
+        UserMapper
+
+
+    }
+
+}
