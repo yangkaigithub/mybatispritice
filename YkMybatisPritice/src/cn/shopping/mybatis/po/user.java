4209a405048fd7c85dba5b9dfe85021a2371253b
yk <ykredhat@163.com>
2016-12-21 09:52:15 +0800

    2016年12月21日09:51:58 修改

--- YkMybatisPritice/src/cn/shopping/mybatis/po/user.java
+++ YkMybatisPritice/src/cn/shopping/mybatis/po/user.java
@@ -1,8 +1,58 @@
 package cn.shopping.mybatis.po;
 
+import java.util.Date;
+
 /**
  * Created by tpusers on 2016/12/20.
  */
 public class user {
-    private
+    private String id;
+    private String username;
+    private String sex;
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+
+    public void setBirthday(Date birthday) {
+        this.birthday = birthday;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    private Date birthday;
+
+    public String getId() {
+        return id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public Date getBirthday() {
+        return birthday;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    private String address;
+
+
 }
--- YkMybatisPritice/test/cn/shopping/mybatis/usertest.java
+++ YkMybatisPritice/test/cn/shopping/mybatis/usertest.java
@@ -1,6 +1,7 @@
 package cn.shopping.mybatis;
 
 
+
 import org.apache.ibatis.io.Resources;
 import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
